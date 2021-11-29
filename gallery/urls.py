15c6dd2f974d84760a79dd7ef550bc34bfd36af1
valentine-ochieng <valentine.ochieng@student.moringaschool.com>
2021-11-29 23:56:18 +0300

    Added media route

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
+app_name = 'effecto'
+
 urlpatterns=[
     path('',views.welcome,name = 'welcome'),
     path('nav/',views.nav,name = 'nav'),
@@ -8,3 +13,5 @@ urlpatterns=[
 
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
