b140e660c3e553852715b6e5e69571d4595b3dbe
valentine-ochieng <valentine.ochieng@student.moringaschool.com>
2021-11-29 21:35:33 +0300

    Added views file

--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+
+def welcome(request):
+    return render (request,'home.html')
+
+
 # Create your views here.
+def nav(request):
+    return render (request,'navbar.html' )
+
+def base(request):
+    return render (request,'base.html' )
