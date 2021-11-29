cb8c8fe4f72243029c6d417df861ad61b63f9462
valentine-ochieng <valentine.ochieng@student.moringaschool.com>
2021-11-30 00:11:01 +0300

    Altered app name

--- gallery/urls.py
+++ gallery/urls.py
@@ -4,13 +4,13 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
-app_name = 'effecto'
+app_name = 'gallery'
 
 urlpatterns=[
     path('',views.welcome,name = 'welcome'),
     path('nav/',views.nav,name = 'nav'),
     path('base/',views.base,name = 'base'),
-    
+
     path(r'^search/', views.search_results, name='search'),
     path(r'^location/(?P<location>\w+)/', views.image_location, name='location'),
 
