243927bae2fec488ea23bcd385713b6bffcbf20b
valentine-ochieng <valentine.ochieng@student.moringaschool.com>
2021-11-30 00:08:15 +0300

    Added location and search paths

--- gallery/urls.py
+++ gallery/urls.py
@@ -10,6 +10,9 @@ urlpatterns=[
     path('',views.welcome,name = 'welcome'),
     path('nav/',views.nav,name = 'nav'),
     path('base/',views.base,name = 'base'),
+    
+    path(r'^search/', views.search_results, name='search'),
+    path(r'^location/(?P<location>\w+)/', views.image_location, name='location'),
 
 ]
 
